78b64c5c8f3064db683d6240b6e1f22073433c67
dayat ambo <hidayatsyahidin5@gmail.com>
2022-12-09 01:49:01 +0700

    live chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,18 +190,28 @@ document.onkeydown = function (e) {
     }
 }
 
-// <!--Start of Tawk.to Script-->
-
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/63922f86daff0e1306dba36e/1gjpgj9tk';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/63922f86daff0e1306dba36e/1gjpgj9tk';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
 })();
-// <!--End of Tawk.to Script-->
+// End of Tawk.to Live Chat
+
+// var Tawk_API = Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/63922f86daff0e1306dba36e/1gjpgj9tk';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+
 
 
 
